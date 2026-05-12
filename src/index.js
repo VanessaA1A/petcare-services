e5b61fea326d41c93b02f3601989b5b5fc089f58
Vanes <ariavila@uamv.edu.ni>
2026-05-12 12:25:21 -0600

    pantalla de login creada con exito

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
@@ -9,6 +10,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/login.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, '../public/login.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, '../public/register.html')));
 
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 
